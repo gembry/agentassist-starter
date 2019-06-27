67752c0a5dc53d7b26c31b55d07e1f597ff39c7b
Greg Embry <greg@fidano.com>
2019-06-26 22:27:43 -0600

    Buyer related

--- models/Buyer.js
+++ models/Buyer.js
@@ -1,14 +1,13 @@
 // Mongoose schema for Buyer
 const mongoose = require("mongoose");
 
-// Create a schema
 const buyerSchema = new mongoose.Schema(
   {
     buyer_address: String,
     buyer_city: String,
     buyer_state: String,
     buyer_zip: Number,
-    buyer_clientIDs: String,
+    buyer_clientIDs:  [{ type : 'ObjectId', required: true }],
     buyer_purchaseprice: Number,
     buyer_earnestprice: Number,
     buyer_concession: Number,
